0006822bb0b521ce313d61c118df06a8fa9dc627
keel <keel.sike@gmail.com>
2016-07-27 01:35:27 +0800

    kconfig增加getConfig方式,去掉KEEL_CONFIG_V

--- test/kconfig.test.js
+++ test/kconfig.test.js
@@ -5,11 +5,12 @@ var expect = require('chai').expect;
 var kconfig = require('../lib/kconfig');
 
 
+var configFilePath = 'test/';
+
 describe('kconfig', function() {
-  var configFilePath = 'test/';
-  beforeEach(function() {
-    delete process.env['KEEL_CONFIG_V'];
-  });
+  // beforeEach(function() {
+  //   delete process.env['KEEL_CONFIG_V'];
+  // });
 
   describe('#readConfig', function() {
     it('should read dev OK', function() {
@@ -19,7 +20,6 @@ describe('kconfig', function() {
       expect('ktool').to.be.eql(re.project);
       expect(6379).to.be.eql(re.redisPort);
       expect('192.168.0.19').to.be.eql(re.redisIP);
-      expect(JSON.stringify(re)).to.be.eql(process.env.KEEL_CONFIG_V);
     });
     it('should read product OK', function() {
       process.env.KTOOL_CONFIG_K = 'product';
@@ -28,7 +28,7 @@ describe('kconfig', function() {
       expect('ktool').to.be.eql(re.project);
       expect(16379).to.be.eql(re.redisPort);
       expect('10.1.1.111').to.be.eql(re.redisIP);
-      expect(JSON.stringify(re)).to.be.eql(process.env.KEEL_CONFIG_V);
+      process.env.KTOOL_CONFIG_K = 'dev';
     });
     it('should be failed by config file', function() {
       var re = kconfig.readConfig(configFilePath + 'configxxx.json');
@@ -49,6 +49,41 @@ describe('kconfig', function() {
     it('should be failed by empty config file', function() {
       var re = kconfig.readConfig(configFilePath + 'config3.json');
       expect(null).to.be.eql(re);
+    });
+  });
+
+  describe('#getConfig', function() {
+
+    var kconfiger = kconfig.kconfig(configFilePath + 'config.json');
+
+    it('should getConfig dev OK', function() {
+      // process.env.KTOOL_CONFIG_K = 'dev';
+      kconfiger.resetReadTimes();
+      var re = kconfiger.getConfig();
+      expect(re).not.to.be.null;
+      expect(1).to.be.eql(re.ver);
+      expect('ktool').to.be.eql(re.project);
+      expect(6379).to.be.eql(re.redisPort);
+      expect('192.168.0.19').to.be.eql(re.redisIP);
+      expect(re.__readTimes).to.be.eql(1);
+    });
+
+    it('should read file only once', function() {
+      // process.env.KTOOL_CONFIG_K = 'dev';
+      kconfiger.resetReadTimes();
+      expect(1).to.be.eql(kconfiger.getConfig().__readTimes);
+      expect(1).to.be.eql(kconfiger.getConfig().__readTimes);
+      expect(1).to.be.eql(kconfiger.getConfig().__readTimes);
+      expect(1).to.be.eql(kconfiger.getConfig().__readTimes);
+    });
+
+    it('should read file force when isForce is true', function() {
+      // process.env.KTOOL_CONFIG_K = 'dev';
+      kconfiger.resetReadTimes();
+      expect(1).to.be.eql(kconfiger.getConfig(true).__readTimes);
+      expect(2).to.be.eql(kconfiger.getConfig(true).__readTimes);
+      expect(3).to.be.eql(kconfiger.getConfig(true).__readTimes);
+      expect(4).to.be.eql(kconfiger.getConfig(true).__readTimes);
     });
   });
 
